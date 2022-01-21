fb1ea7b300d3cafcb3b784d1aaff84cc3c94046a
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-01-21 09:09:46 +0100

    modify - HomeController.php in controllers

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -11,6 +11,8 @@ class HomeController
         if(!empty($_POST) and !empty($_POST['password']) and !empty($_POST['submitPassword'])){
             $status = $this->checkPassword($_POST['password']);
         }
+
+        require_once(VIEW_PATH . 'home.php');
     }
 
     public function checkPassword(string $password) : string {
