29035ea31cfa4abc48a9d2b8af9a461a8ce325a1
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-01-21 08:40:18 +0100

    add - minimal code in index.php to be functionnal

--- index.php
+++ index.php
@@ -3,3 +3,12 @@
 define('VIEW_PATH', 'views/');
 define('CONTROLLERS', 'controllers/');
 
+$action = (isset($_GET['action'])) ? htmlentities($_GET['action']) : 'default';
+switch($action){
+    default:
+        require_once ('controllers/HomeController.php');
+        $controller = new HomeController();
+        break;
+}
+
+$controller->run();
