00d3494df0fd69ac12a197440149d5a74eca7a9f
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-01-21 08:50:52 +0100

    Add - minimal code in controllers/HomeController.php to be functionnal

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -3,5 +3,17 @@
 
 class HomeController
 {
+    public function __construct(){
 
+    }
+
+    public function run(){
+        if(!empty($_POST) and !empty($_POST['password']) and !empty($_POST['submitPassword'])){
+            $status = $this->checkPassword($_POST['password']);
+        }
+    }
+
+    public function checkPassword(string $password) : string {
+        return "";
+    }
 }
