2e40ac231bf279df66bbec65e23536b72ea3433d
Jonathan Van de Vyver <jonathan.vandevyver@student.vinci.be>
2022-01-21 08:56:32 +0100

    add - views/header.php minimal code to be functionnal

---
+++ views/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset=""utf-8>
+        <title>IPL social </title>
+    </head>
+    <body>
+    <div id="global">
+        <div id="entete">
+            <h1>
+                <a href="index.php"></a>
+                Le site pour l'examen de DevOps Janvier 2022
+            </h1>
+            <p class="sous-titre">
+                <strong>Institut Paul Lambin </strong>
+                :: 2ième année du baccalauréat en informatique
+            </p>
+        </div><!--entete-->
